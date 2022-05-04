a101592c7dfcbdec4962ec1c7d2080c4e67e9f2f
STANGENSELLERIE <vinix.link@gmail.com>
2022-05-04 14:45:06 +0800

    add base python structure

---
+++ app.py
@@ -0,0 +1,27 @@
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+from PyQt5.uic import loadUiType
+
+import sys
+import os
+
+ui, _ = loadUiType('main.ui')
+
+
+class MainApp(QMainWindow, ui):
+    def __init__(self, parent=None):
+        super(MainApp, self).__init__(parent)
+        QMainWindow.__init__(self)
+        self.setupUi(self)
+
+
+def main():
+    app = QApplication(sys.argv)
+    window = MainApp()
+    window.show()
+    app.exec_()
+
+
+if __name__ == '__main__':
+    main()
