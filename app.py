c52dc49b04bed59ec4609d1b5f8ad4212291deef
STANGENSELLERIE <vinix.link@gmail.com>
2022-05-06 22:17:24 +0800

    add file system browse for choose save location

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from PyQt5.uic import loadUiType
 
 import sys
 import os
+import urllib.request
 
 ui, _ = loadUiType('main.ui')
 
@@ -24,14 +25,21 @@ class MainApp(QMainWindow, ui):
     def buttons_handler(self):
         # handle buttons in app
         self.pushButton.clicked.connect(self.download) # connect download button in ui w/ download method
+        self.pushButton_2.clicked.connect(self.browser_handler)
 
-    def progress_handler(self):
+    def progress_handler(self, block_num, block_size, total_size):
         # progress bar
-        pass
+        readed_data = block_num * block_size
 
-    def browse_handler(self):
+        if total_size > 0:
+            download_percentage = readed_data * 100 / total_size
+            self.progressBar.setValue(download_percentage)
+            QApplication.processEvents()
+
+    def browser_handler(self):
         # os file system browse to choose save location (by pic)
-        pass
+        save_location = QFileDialog.getSaveFileName(self, caption="Save as", directory=".", filter="All Files(*.*)")
+        self.lineEdit_2.setText(str(save_location[0]))
 
     def save(self):
         # enter specific save location (by writing)
@@ -40,6 +48,11 @@ class MainApp(QMainWindow, ui):
     def download(self):
         # download file
         print('Starting Download...')
+
+        download_url = self.lineEdit.text()
+        save_location = self.lineEdit_2.text()
+
+        urllib.request.urlretrieve(download_url, save_location, self.progress_handler)
 
 
 def main():
