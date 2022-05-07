466cd17d01ead4c36c16bf10110f43f2cfa2950d
STANGENSELLERIE <vinix.link@gmail.com>
2022-05-07 20:51:50 +0800

    add method for download yt video and method for progress bar

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ class MainApp(QMainWindow, ui):
         # contain all ui changes
         pass
 
-    def buttons_handler(self):
+    def buttons_handler(self) -> None:
         # register buttons in app
         # download files
         self.pushButton.clicked.connect(self.download) # connect download button in ui w/ download method
@@ -33,9 +33,10 @@ class MainApp(QMainWindow, ui):
 
         # download single yt video
         self.pushButton_5.clicked.connect(self.get_video_data)
+        self.pushButton_3.clicked.connect(self.save_browse)
+        self.pushButton_4.clicked.connect(self.download_video)
 
-    # methods for downloading files by direct link
-    def progress_handler(self, block_num, block_size, total_size):
+    def progress_handler(self, block_num: int, block_size: int, total_size: int) -> None:
         # progress bar
         readed_data = block_num * block_size
 
@@ -44,7 +45,10 @@ class MainApp(QMainWindow, ui):
             self.progressBar.setValue(int(download_percentage))
             QApplication.processEvents()
 
-    def browser_handler(self):
+    # --------------------------------------------
+    # methods for downloading files by direct link
+    # --------------------------------------------
+    def browser_handler(self) -> None:
         # os file system browse to choose save location (by pic)
         save_location = QFileDialog.getSaveFileName(self, caption="Save as", directory=".", filter="All Files(*.*)")
         self.lineEdit_2.setText(str(save_location[0]))
@@ -53,7 +57,7 @@ class MainApp(QMainWindow, ui):
         # enter specific save location (by writing)
         pass
 
-    def download(self):
+    def download(self) -> None:
         # download file by direct link
         download_url = self.lineEdit.text()
         save_location = self.lineEdit_2.text()
@@ -66,7 +70,7 @@ class MainApp(QMainWindow, ui):
                 urllib.request.urlretrieve(download_url, save_location, self.progress_handler)
             except Exception:
                 QMessageBox.warning(self, 'Download Error', 'Enter a valid URL or save location')
-                return
+                return None
 
         QMessageBox.information(self, 'Download Completed', 'Download Completed Successfully')
 
@@ -75,7 +79,14 @@ class MainApp(QMainWindow, ui):
         self.lineEdit_2.setText('')
         self.progressBar.setValue(0)
 
+    # ---------------------------------------------
     # methods for downloading youtube single video
+    # ---------------------------------------------
+    def save_browse(self):
+        # os file system browse to choose save location
+        save_location = QFileDialog.getSaveFileName(self, caption="Save as", directory=".", filter="All Files(*.*)")
+        self.lineEdit_4.setText(str(save_location[0]))
+
     def get_video_data(self):
         # information about video (quality, size, ...) in order to select a specific quality
         video_url = self.lineEdit_3.text()
@@ -91,10 +102,27 @@ class MainApp(QMainWindow, ui):
                 self.comboBox.addItem(data)
 
     def download_video(self):
-        pass
+        video_url = self.lineEdit_3.text()
+        save_location = self.lineEdit_4.text()
 
-    def download_progress(self):
-        pass
+        if video_url == '' or save_location == '':
+            QMessageBox.warning(self, 'Error', 'Enter fields should not be empty')
+
+        else:
+            video = pafy.new(video_url)
+            video_stream = video.videostreams
+            video_quality = self.comboBox.currentIndex()
+            video_stream[video_quality].download(filepath=save_location, callback=self.download_progress)
+
+    def download_progress(self, total: int, received: int, ratio: int, rate: int, time: int) -> None:
+        read_data = received
+        if total > 0:
+            download_percentage = int(read_data * 100 / total)
+            self.progressBar_2.setValue(int(download_percentage))
+            remaining_time = int(round(time / 60, 2))
+
+            self.label_5.setText(str(f'{remaining_time} minutes remaining'))
+            QApplication.processEvents()
 
 
 def main():
